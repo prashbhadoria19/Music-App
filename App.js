e329c191c27e639951ede42cdbb6291bf670c572
prashbhadoria19 <136710763+prashbhadoria19@users.noreply.github.com>
2023-06-15 18:43:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.maintext}>GLA University, Mathura</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  maintext:{
+    fontSize:20,
+    fontStyle:'bold',
+    
+
+  }
+});
